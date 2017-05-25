b66292a183b4bf71ee7b25bf7d1938597bebe0b9
Ender187 <m.hiltawski@hotmail.com>
2017-05-25 17:40:52 +0200

    poprawione dzialanie carousel

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ $(function(){
 	var carouselList = $("#carousel ul");
 	setTimeout(changeSlide, 3000); 
 	setInterval(changeSlide, 3000); 
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+	
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
 });

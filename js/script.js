d6ff164ade7cf56de427b888618fa68478d6798c
Ender187 <m.hiltawski@hotmail.com>
2017-05-25 12:30:00 +0200

    pierwszy commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	setTimeout(changeSlide, 3000); 
+	setInterval(changeSlide, 3000); 
+	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+});
